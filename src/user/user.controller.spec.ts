aaf4c1983bef48379b5a931ce555be03820f786e
CanberkAkar <akarcanberk@gmail.com>
2025-03-18 14:08:06 +0300

    Updated test user controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            activeUsers: jest.fn().mockReturnValue('Mocked Active Users'),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return active users', () => {
+    expect(controller.activeUsers()).toBe('Mocked Active Users');
   });
 });
