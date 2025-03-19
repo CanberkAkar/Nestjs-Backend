74617e472fa28bc067cd7bb9cae30eb7d94c1791
CanberkAkar <akarcanberk@gmail.com>
2025-03-19 12:17:56 +0300

    Updated Main ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe)
+  const config = new DocumentBuilder()
+  .setTitle('USER-ORDERING SYSTEM API')
+  .setDescription('NESTJS-BACKEND')
+  .setVersion('1.0')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
