11f35a70e56d4a12f3320df1744a721d1f40970c
CanberkAkar <akarcanberk@gmail.com>
2025-03-19 12:18:46 +0300

    Updated user module and user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Repository } from 'typeorm';
+import { Users } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuidv4 } from 'uuid';
+function generateKey(): string {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    
+    function getRandomSegment(length: number): string {
+        return Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
+    }
 
-@Injectable()
-export class UserService {
-    activeUsers(){
+    return `${getRandomSegment(4)}-${getRandomSegment(4)}-${getRandomSegment(4)}-${getRandomSegment(4)}`;
+}
+    @Injectable()
+    export class UserService 
+    {
+        
+    private readonly logger = new Logger(UserService.name);  // Logger'ı sınıf içinde enjekte ettik
+
+    constructor(@InjectRepository(Users) private readonly userRepository: Repository<Users>) {}
+
+    activeUsers() {
         return 'Active users';
     }
-    createUser(createUserDto:CreateUserDto){
-        return { status: "201", "User":createUserDto };
-    }  
+
+    async createUser(createUserDto: CreateUserDto) {
+        this.logger.log('Attempting to create a new user');   
+        
+        try {
+            const apiKey = generateKey();
+            const users = this.userRepository.create(createUserDto);  
+            users.usr_id = uuidv4();
+            users.usr_email = createUserDto.usr_email;
+            users.usr_name = createUserDto.usr_name;
+            users.usr_token = apiKey;
+            users.createdAt = new Date();
+            users.updatedAt = new Date();
+
+            await this.userRepository.save(users);   
+            
+            this.logger.log(`User created successfully with ID: ${users.usr_id}`); 
+            return { status: "201", user: users };
+        } catch (error) {
+            this.logger.error('Error occurred while creating user', error.stack);
+            throw error;  // Hata fırlatma
+        }
+    }
 }
