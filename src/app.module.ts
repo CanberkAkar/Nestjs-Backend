046cf124c015f305992c27b1d50bb5c5ba4909ef
CanberkAkar <akarcanberk@gmail.com>
2025-03-19 12:13:28 +0300

    Updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,  
+      envFilePath: '.env',  
+    }),
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:process.env.DB_HOST,
+      port:parseInt(process.env.DB_PORT || '3306'),
+      username:process.env.DB_USERNAME,
+      password:process.env.DB_PASSWORD,
+      database:process.env.DB_DATABASE,
+      entities:[Users],
+      synchronize:process.env.DB_SYNC == 'true'
+    }),
+    TypeOrmModule.forFeature([Users]),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
