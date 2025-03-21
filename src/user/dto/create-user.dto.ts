d50ab427318a38dfb3412733e8da8dd17357df8b
CanberkAkar <akarcanberk@gmail.com>
2025-03-21 10:54:57 +0300

    Updated user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -3,6 +3,7 @@ import { IsEmail,IsNotEmpty,IsString } from 'class-validator';
 import { ApiProperty, } from '@nestjs/swagger';
 
 export class CreateUserDto{
+    //EKLEME İÇİN KULLANACAĞIMIZ DTO'LAR
     @IsNotEmpty()
     @IsString()
     @ApiProperty({
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -4,6 +4,8 @@ import { ApiProperty, } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends CreateUserDto{
+    //EKLEME İÇİN KULLANACAĞIMIZ DTO'LAR BURADA EXTENDS ETME İŞLEMİ YAPTIK. BU SAYEDE CREATEUSERDTO'DAKİ ALANLARI KULLANABİLİRİZ
+
     @IsNotEmpty()
     @IsNumber()
     @ApiProperty({
