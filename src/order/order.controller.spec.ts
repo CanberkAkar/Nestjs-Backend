4689e631e2893a0d21587e5070da17e7febe1bad
CanberkAkar <akarcanberk@gmail.com>
2025-03-21 12:01:36 +0300

    Test files updated

---
+++ src/order/order.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+
+describe('OrderController', () => {
+  let controller: OrderController;
+  let service: OrderService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [OrderController],
+      providers: [
+        {
+          provide: OrderService,
+          useValue: {
+            list: jest.fn().mockReturnValue('Mocked Orders'),
+          },
+        }
+      ],
+    }).compile();
+
+    controller = module.get<OrderController>(OrderController);
+    service = module.get<OrderService>(OrderService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+  it('should return order list', () => {
+    expect(controller.list()).toBe('Order list');
+  });
+});
